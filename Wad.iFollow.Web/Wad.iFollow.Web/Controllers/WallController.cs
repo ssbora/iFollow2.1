968e0202a86433f5697d47ae5702e6831e51208e
ssbora <sorin_justme@yahoo.com>
2013-12-15 10:53:54 +0200

    iFollow2.1
    Version 2.1

---
+++ Wad.iFollow.Web/Wad.iFollow.Web/Controllers/WallController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Wad.iFollow.Web.Models;
+
+namespace Wad.iFollow.Web.Controllers
+{
+    public class WallController : Controller
+    {
+        //
+        // GET: /Main/
+
+        public ActionResult MainPage()
+        {
+            user currentUser = Session["user"] as user;
+            WallPostsModel wpm = new WallPostsModel();
+
+            using (var entities = new ifollowdatabaseEntities4())
+            {
+                currentUser = entities.users.First(u => u.id == currentUser.id);
+                wpm.BuildFromImagesAndPosts(currentUser.posts, currentUser.images);
+            }
+
+            return View(wpm);          
+        }
+
+        public ActionResult WallPost()
+        {
+            return PartialView("_WallPosts");
+        }
+
+        public ActionResult Settings()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return PartialView("_SettingsModal");
+        }
+
+        public ActionResult Post()
+        {
+            ViewBag.Message = "Your posting page.";
+            return PartialView("_PostSettings");
+        }
+
+        public ActionResult Profile()
+        {
+            user currentUser = Session["user"] as user;
+            ProfileModel pm = new ProfileModel();
+            pm.userName = currentUser.firstName;
+
+            using (var entities = new ifollowdatabaseEntities4())
+            {
+                currentUser = entities.users.First(u => u.id == currentUser.id);
+                pm.postsCount = currentUser.posts.Count();
+                pm.followersCount = currentUser.followers.Count();
+                pm.followedCount = currentUser.followers1.Count();
+                pm.elements.BuildFromImagesAndPosts(currentUser.posts, currentUser.images);
+            }
+            ViewBag.Message = "Your profile page.";
+            return PartialView("_ProfilePage", pm);
+        }
+
+        public ActionResult Followers()
+        {
+            ViewBag.Message = "Your followers page.";
+            return PartialView("_Followers");
+        }
+    }
+}
---
+++ Wad.iFollow.Web/Wad.iFollow.Web/Models/WallPostsModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.Diagnostics;
+
+namespace Wad.iFollow.Web.Models
+{
+    public class WallPostsModel
+    {
+        public List<UploadFileModel> wallElements { get; set; }
+
+        public void BuildFromImagesAndPosts(ICollection<post> posts, ICollection<image> images)
+        {
+            wallElements = new List<UploadFileModel>();
+
+            for (int index = posts.Count() - 1; index >= 0 ; index--)
+            {
+                UploadFileModel newModel = new UploadFileModel();
+                newModel.Message = posts.ElementAt(index).message;
+
+                if (posts.ElementAt(index).imageId != null)
+                {
+                    string imagePath = images.First(i => i.id == posts.ElementAt(index).imageId).url;
+                    string path = @"~/Images/UserPhotos/" + imagePath;
+                    newModel.Path = path;
+                }
+                else
+                {
+                    newModel.Path = "~/Images/1.jpg";
+                }
+
+                wallElements.Add(newModel);
+            }
+        }
+    }
+}
